a29349b28478fdd2facc2bacfcbc1580c0edc792
gstearmit@gmail.com <gstearmit@gmail.com@6b512c24-98fc-c699-5e5f-3fd35318231b>
2014-08-11 02:15:00 +0000

    update admin

--- admin/app_controller.php
+++ admin/app_controller.php
@@ -10,7 +10,30 @@ class AppController extends Controller {
 	'Time',	
 	'Javascript'	
     );
-   
+ function beforeFilter() {
+ 
+ 	$urlTmp = $_SERVER['REQUEST_URI'];
+ 	if (stripos($urlTmp, "?languages")) {
+ 		$urlTmp = explode("?", $urlTmp);
+ 		$lang = explode("=", $urlTmp[1]);
+ 		$lang = $lang[1];
+ 		if (isset($lang)) {
+ 			$this->Session->write('languages', $lang);
+ 		} else {
+ 			$this->Session->delete('languages');
+ 		}
+ 		
+ 	}
+ 
+ 	// set language
+ 	if($this->Session->read('languages') == 'vie' || $this->Session->read('languages') == "") {
+ 		Configure::write('Config.languages', 'vie');
+ 	} else {
+ 		Configure::write('Config.languages', $this->Session->read('languages'));
+ 	}
+ 
+ 
+    }
 }
 
 ?>
