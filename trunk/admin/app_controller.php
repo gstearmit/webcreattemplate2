1a3eb012cf15422d6b97b4e335f16fa2f3050734
gstearmit@gmail.com <gstearmit@gmail.com@6b512c24-98fc-c699-5e5f-3fd35318231b>
2014-08-11 08:33:09 +0000

    Translate Done fix

--- trunk/admin/app_controller.php
+++ trunk/admin/app_controller.php
@@ -34,6 +34,43 @@ class AppController extends Controller {
  
  
     }
+ var $_sessionUsername  = "Username";
+ var $_sessionID = 'id';
+ var $layout = false;
+// var $uses = array('Config','Category','Banner','News','Video','Gallery'); // 'Group','Catnew','Factory','Level','Support','Introduce',
+ 
+ 
+//  function beforeFilter() {
+ 	
+//  //	die("controllller");
+ 	
+//  	$this->layout = 'admin';
+// 	$urlTmp = $_SERVER['REQUEST_URI'];
+	
+// 	pr($urlTmp);die;
+	
+//  	if (stripos($urlTmp, "?language")) {
+//  		$urlTmp = explode("?", $urlTmp);
+//  		$lang = explode("=", $urlTmp[1]);
+//  		$lang = $lang[1];
+//  		if (isset($lang)) {
+//  			$this->Session->write('language', $lang);
+//  		} else {
+//  			$this->Session->delete('language');
+//  		}
+//  	}
+ 
+//  	// set language
+//  	if($this->Session->read('language') == 'vie' || $this->Session->read('language') == "") {
+//  		Configure::write('Config.language', 'vie');
+//  	} else {
+//  		Configure::write('Config.language', $this->Session->read('language'));
+//  	}
+ 
+ 
+//  }
+ 
+ 
 }
 
 ?>
--- trunk/admin/app_model.php
+++ trunk/admin/app_model.php
@@ -35,58 +35,54 @@ class AppModel extends Model {
 	function setLanguage($locale) {
 		$this->locale = $locale;
 	}
-// 	public function setDatabase($database, $datasource = 'default')
-//     {
-//       $nds = $datasource ;//. '_' . $database;      
-//       $db  = &ConnectionManager::getDataSource($datasource);
-
-//       $db->setConfig(array(
-//         'name'       => $nds,
-//         'database'   => $database,
-//         'persistent' => false
-//       ));
-
-// 	  //return $db->config;die;
-//       if ( $ds = ConnectionManager::create($nds, $db->config) ) {
-//         $this->useDbConfig  = $nds;
-//         $this->cacheQueries = false;
-//         return true;
-//       }
-
-//       return false;
-//     }
+	function setDataEshop($hostname,$username,$password,$databasename)
+	{
+		//++++++++++++++++++++++++++++++++++++++++
+		$db = new ConnectionManager;
+		$config = array(
+				//'className' => 'Cake\Database\Connection',
+				'driver' => 'mysql',
+				'persistent' => false,
+				'host' =>$hostname,
+				'login' =>$username,
+				'password' =>$password,
+				'database' =>$databasename,
+				'prefix' => false,
+				'encoding' => 'utf8',
+				'timezone' => 'UTC',
+				'cacheMetadata' => true
+		);
+		$db->create($databasename,$config);
 	
-// 	public function setDatabaseTwoNew($database, $datasource = 'default')
-// 	{
-//             $db_config = ConnectionManager::getDataSource('default')->config;
-             
-//             $link = mysql_connect($db_config['host'], $db_config['login'], $db_config['password']);
-//             mysql_select_db($db_config['database'], $link);
-//             $res = mysql_query('SELECT * FROM ecosstore WHERE 1=1');
-//             $row = mysql_fetch_assoc($res);
-             
-//             if(isset($row['hostname'])){
-//                 $dbName = $row['database_name'];
-// 				if( $database === $dbName )
-// 				{
-//                 $config = array();
-//                 // Set correct database name
-//                 $config['driver'] = 'mysql';
-//                 $config['persistent'] = false;
-//                 $config['hostname'] = $row['hostname'];
-//                 $config['username'] = $row['username'];
-//                 $config['password'] = $row['password'];
-//                 $config['database'] = $dbName;
-//                 $config['prefix'] = '';
-                 
-//                 // Add new config to registry
-//                 ConnectionManager::create($dbName, $config);
-//                 // Point model to new config
-//                 $this->useDbConfig = $dbName;
-// 				// return true;
-// 				return $dbName;
-// 				}
-// 	       }
-// 		   return false;	
-//     }
+		if ( $name = ConnectionManager::getDataSource($databasename)) {
+			$this->useDbConfig  = $databasename;
+			$this->cacheQueries = false;
+			return true;
+		}
+	
+		return false;
+	}
+	
+	
+	public function setDatabase($database, $datasource = 'default')
+	{
+		$nds = $database;
+		$db  = &ConnectionManager::getDataSource($datasource);
+	
+		$db->setConfig(array(
+				'name'       => $nds,
+				'database'   => $database,
+				'persistent' => false
+		));
+	
+		if ( $ds = ConnectionManager::create($nds, $db->config) ) {
+			$this->useDbConfig  = $nds;
+			$this->cacheQueries = false;
+			return true;
+		}
+	
+		return false;
+	
+		 
+	}
 }
